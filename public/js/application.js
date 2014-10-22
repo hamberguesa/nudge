7fd16d28f5739c48b666a9398fd2b49456f63e1f
Amber Hager <ambermhager@gmail.com>
2014-10-22 15:57:16 -0700

    ajax call on nudges page working

--- public/js/application.js
+++ public/js/application.js
@@ -10,11 +10,14 @@ var confirm_nudge = function(evt) {
 	$.ajax({
 		url: 'nudges/create',
 		type: 'POST',
-		data: data
-	}).done(view.update(returned_nudge))
+		data: data, 
+		success: function(returned_nudge) {
+			$('form').hide();
+			var sentence = "Your nudge will be sent at" + returned_nudge["datetime"] + " to " + returned_nudge["receiver_name"]
+			$('.nudge_container').append("<br><br><p>" + sentence + "</p>");
+		}
+	});
 };
-
-
 
 // the below should probably go in the view
 
--- public/js/view.js
+++ public/js/view.js
@@ -1,9 +1,4 @@
 function View() {
 
-	this.update= function(returned_nudge) {
-		$('form').hide();
-		var sentence = "Your nudge will be sent at" + returned_nudge["datetime"] + " to " + returned_nudge["receiver_name"]
-		$('.nudge_container').append("<br><br><p>" + sentence + "</p>");
-	};
 };
 
