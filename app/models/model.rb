dcc6aa6beaa278b1d54ec86d967f68f93aeacd1a
Amber Hager <ambermhager@gmail.com>
2014-10-23 15:59:29 -0700

    got texts working

--- app/models/model.rb
+++ app/models/model.rb
@@ -12,7 +12,8 @@ class Worker
 	account_sid = ENV['ACCT_SID']
 	auth_token = ENV['AUTH_TOKEN']
 
-
+	account_sid = 'AC6090c1b7becb69836694649bdf2dc4ee'
+	auth_token = '608b52f087b35b81bb5d1ac556715945'
 
 	@client = Twilio::REST::Client.new account_sid, auth_token 
 
@@ -21,14 +22,15 @@ class Worker
 		now = Time.new
 
 		nudges.each do |nudge|
-			nudge_time = Time.parse(nudge.datetime)
+			# nudge_time = Time.parse(nudge.datetime)
+			nudge_time = nudge.datetime
 			if nudge_time > (now - 150000) && nudge_time < (now + 150000) 
 				send_text(nudge.phone_num, nudge.message)
 			end
 		end
 	end
 
-	def send_text(phone_num, message)
+	def self.send_text(phone_num, message)
 		@client.account.messages.create({
 			:from => '+14159677811', 
 			:to => phone_num, 
